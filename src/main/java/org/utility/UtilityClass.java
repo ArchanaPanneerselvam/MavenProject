420dbfb194183e5b16134db59cfc4a66ea562eee
archiselvam057 <archiselvam057@gmail.com>
2025-04-22 15:20:26 +0530

    Mahi's code

--- src/main/java/org/utility/UtilityClass.java
+++ src/main/java/org/utility/UtilityClass.java
@@ -4,5 +4,11 @@ public class UtilityClass {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("Test4");
+		System.out.println("Test5");
+		System.out.println("Test6");
+		System.out.println("Test7");
+
+
 	}
 }
